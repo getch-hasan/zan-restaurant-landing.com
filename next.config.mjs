4adced60a8007214bb7d679e7c09b18e3ba195a2
getch-hasan <hasan15-3112@diu.edu.bd>
2025-01-09 10:14:14 +0600

    dd

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-  
+  images: {
+    unoptimized: true, // Disable image optimization for static export
+  },
 };
 
 export default nextConfig;
